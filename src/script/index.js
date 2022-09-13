8fa5b8e409323e1ecbbe25aea1a1b39ee639d2b6
Grizz1e <thejackshiva@gmail.com>
2022-09-13 16:33:27 +0545

    Lets get started

---
+++ src/script/index.js
@@ -0,0 +1,32 @@
+const menuBtn = document.getElementById("menu-btn")
+const leftContainer = document.getElementById("left-container")
+const searchBar = document.getElementById("search-bar")
+const centerNav = document.getElementById("center-nav")
+const homepageMain = document.getElementById("home-area")
+const vidBox = document.querySelectorAll('.vid-box')
+
+menuBtn.addEventListener('click', () => {
+    leftContainer.classList.toggle("hidden")
+    homepageMain.classList.toggle("menu-closed")
+})
+
+searchBar.addEventListener("focusin", () => {
+    centerNav.style.border = "4px solid #de0151"
+})
+searchBar.addEventListener("focusout", () => {
+    centerNav.style.border = "none"
+})
+centerNav.addEventListener("mouseover", () => {
+    centerNav.style.border = "4px solid #de0151"
+})
+centerNav.addEventListener("mouseout", () => {
+    if (document.activeElement !== searchBar) {
+        centerNav.style.border = "none"
+    }
+})
+
+vidBox.forEach(vid => {
+    vid.addEventListener('click', () => {
+        window.location = '/src/html/watch.html'
+    })
+})
