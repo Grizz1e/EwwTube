101641b35779f5a37a93abdf1be02f0bf0e0f389
Grizz1e <thejackshiva@gmail.com>
2022-09-13 21:44:48 +0545

    added signin page

--- src/script/index.js
+++ src/script/index.js
@@ -4,6 +4,8 @@ const searchBar = document.getElementById("search-bar")
 const centerNav = document.getElementById("center-nav")
 const homepageMain = document.getElementById("home-area")
 const vidBox = document.querySelectorAll('.vid-box')
+const accBtn = document.getElementById('account-btn')
+const accMenu = document.getElementById('account-menu')
 
 menuBtn.addEventListener('click', () => {
     leftContainer.classList.toggle("hidden")
@@ -24,7 +26,11 @@ centerNav.addEventListener("mouseout", () => {
         centerNav.style.border = "none"
     }
 })
-
+accBtn.addEventListener('click', () => {
+    document.body.classList.toggle('overflow-hidden')
+    accBtn.classList.toggle('text-highlight')
+    accMenu.classList.toggle('hidden')
+})
 vidBox.forEach(vid => {
     vid.addEventListener('click', () => {
         window.location = '/watch'
